fa9b9df2eff30ccaa3a7aca6c9b695a782427f3c
yang <2687518378@qq.com>
2019-06-06 21:14:44 +0800

    5

--- src/main/java/com/qfedu/travelsys1901/serice/ViewService.java
+++ src/main/java/com/qfedu/travelsys1901/serice/ViewService.java
@@ -1,8 +1,9 @@
 package com.qfedu.travelsys1901.serice;
 
 import com.qfedu.travelsys1901.entity.View;
+import com.qfedu.travelsys1901.vo.JsonBean;
 
 public interface ViewService {
 
-    View selectByPrimaryKey(Integer vid);
+    JsonBean selectByPrimaryKey(Integer vid);
 }
--- src/main/java/com/qfedu/travelsys1901/service/impl/ViewServiceImpl.java
+++ src/main/java/com/qfedu/travelsys1901/service/impl/ViewServiceImpl.java
@@ -1,4 +1,29 @@
 package com.qfedu.travelsys1901.service.impl;
 
-public class ViewServiceImpl {
+import com.qfedu.travelsys1901.dao.ViewMapper;
+import com.qfedu.travelsys1901.entity.View;
+import com.qfedu.travelsys1901.serice.ViewService;
+import com.qfedu.travelsys1901.vo.JsonBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ViewServiceImpl implements ViewService {
+
+    @Autowired
+    private ViewMapper viewDao;
+    @Override
+    public JsonBean selectByPrimaryKey(Integer vid) {
+
+
+        try {
+            View view = viewDao.selectByPrimaryKey(vid);
+            return new JsonBean(1,view);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return new JsonBean(0,e.getMessage());
+        }
+
+    }
 }
