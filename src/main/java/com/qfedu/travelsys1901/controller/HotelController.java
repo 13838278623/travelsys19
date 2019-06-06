9caa7f1e053dc5a55e3d287cafdbc2153c21af1f
yang <2687518378@qq.com>
2019-06-06 11:05:25 +0800

    3

--- src/main/java/com/qfedu/travelsys1901/controller/HotelController.java
+++ src/main/java/com/qfedu/travelsys1901/controller/HotelController.java
@@ -1,4 +1,32 @@
 package com.qfedu.travelsys1901.controller;
 
+
+import com.qfedu.travelsys1901.entity.Hotel;
+import com.qfedu.travelsys1901.serice.HotelService;
+import com.qfedu.travelsys1901.vo.JsonBean;
+import com.qfedu.travelsys1901.vo.VHotel;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Api(value = "酒店操作",tags = "酒店操作")
+@ResponseBody
+@Controller
 public class HotelController {
+
+    @Autowired
+    private HotelService hotelService;
+    @CrossOrigin
+    @ApiOperation(value = "酒店订单",notes = "订单一个用户")
+    @RequestMapping(value = "/hote/add.do",method = RequestMethod.PUT)
+    public JsonBean add(VHotel vHotel){
+
+      return   hotelService.insertSelective(vHotel);
+
+    }
 }
---
+++ src/main/java/com/qfedu/travelsys1901/service/impl/HotelServiceImpl.java
@@ -0,0 +1,52 @@
+package com.qfedu.travelsys1901.service.impl;
+
+import com.qfedu.travelsys1901.dao.HotelMapper;
+import com.qfedu.travelsys1901.dao.ShopMapper;
+import com.qfedu.travelsys1901.dao.UserMapper;
+import com.qfedu.travelsys1901.entity.Hotel;
+import com.qfedu.travelsys1901.entity.Shop;
+import com.qfedu.travelsys1901.entity.User;
+import com.qfedu.travelsys1901.serice.HotelService;
+import com.qfedu.travelsys1901.vo.JsonBean;
+import com.qfedu.travelsys1901.vo.VHotel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HotelServiceImpl implements HotelService {
+
+    @Autowired
+    private HotelMapper hotelDao;
+    @Autowired
+    private UserMapper  userDao;
+    @Override
+    public JsonBean  insertSelective(VHotel vHotel) {
+        Hotel hotel = new Hotel();
+        User user = new User();
+
+        hotel.setTinfo(vHotel.getTinfo());
+        hotel.setHid(vHotel.getHid());
+        hotel.setHincity(vHotel.getHincity());
+        hotel.setHindate(vHotel.getHindate());
+        hotel.setHsite(vHotel.getHsite());
+        hotel.setT_Hotelcol(vHotel.getT_hotelcol());
+        hotel.setHoutdate(vHotel.getHoutdate());
+
+        user.setUaddress(vHotel.getUaddress());
+        user.setUemail( vHotel.getUemail());
+        user.setUname(vHotel.getUname());
+        user.setUphone(vHotel.getUphone());
+
+        try {
+            hotelDao.insertSelective(hotel);
+            userDao.insertSelective(user);
+            return new JsonBean(1,null);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return new JsonBean(0,e.getMessage());
+        }
+
+    }
+
+}
